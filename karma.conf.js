c7bab49b7c95b9b154916db0061a827110359216
Anwar <Anwar@DESKTOP-0OH6CLM>
2022-03-13 01:38:20 +0200

    change names

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cex'),
+      dir: require('path').join(__dirname, './coverage/easti'),
       subdir: '.',
       reporters: [
         { type: 'html' },
